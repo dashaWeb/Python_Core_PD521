fe3560df8d58c21bb2048edbf5a7a4c157f15610
Dasha <dashakonopelko@gmail.com>
2025-06-02 16:51:19 +0300

    Python intro

---
+++ 01_intro.py
@@ -0,0 +1,6 @@
+# print('\t\tHe\allo \\"World"\\ \n'+"\"Python\" Itst\bepAcademy")
+# print("Lorem ipsum dolor")
+
+# print("Hello", end=" ")
+# print("Python")
+print("\tA","B","C","D", sep="\n\t")
