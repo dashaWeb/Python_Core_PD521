da879975a9603306c72ac7557eb822b23285aa4a
Dasha <dashakonopelko@gmail.com>
2025-06-02 17:52:05 +0300

    colors

--- 01_intro.py
+++ 01_intro.py
@@ -1,6 +1,18 @@
 # print('\t\tHe\allo \\"World"\\ \n'+"\"Python\" Itst\bepAcademy")
-# print("Lorem ipsum dolor")
+# print("\033[31m\033[42mLorem \033[0m ipsum dolor")
 
 # print("Hello", end=" ")
 # print("Python")
-print("\tA","B","C","D", sep="\n\t")
+# print("\tA","B","C","D", sep="\n\t")
+
+print("\033[1mTask 1\033[0m")
+print("\033[31mNothing \033[0m\nwill \033[31mwork \033[0m\nunless you \033[31mdo\033[0m")
+
+
+print("\033[1mTask 2\033[0m")
+print(' "\033[35mAnyone who\33[0m \n\tstops \n\tlearning \033[31mis\033[0m old, \n\twether at twenty \033[33mor\033[0m eighty" \n\tHenry Ford')
+
+
+print("\033[1mTask 3\033[0m")
+print("\033[35mSuccess is not the key to happiness.\033[0m")
+print("\tHappiness\n\t\t\033[31mis\033[0m\n\t\t\tthe\n\t\t\t\tkey\n\t\t\t\t\t\033[31mto\033[0m\n\t\t\t\t\t\tsuccess.")
